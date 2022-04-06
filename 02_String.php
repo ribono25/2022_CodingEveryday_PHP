bc7bdbe1ce209c594096faa0b3f911fb1b193140
SeongHyeon Kim <94503162+ribono25@users.noreply.github.com>
2022-04-06 22:40:14 +0900

    Add files via upload

---
+++ 02_String.php
@@ -0,0 +1,24 @@
+<!-- 02_String.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>String & String operators</h1>
+    <?php
+    echo "Hello \"w\"orld";
+    ?>
+    <h2>concatenation operators</h2>
+    <?php
+    echo "Hello "."world";
+    ?>
+    <h2>String length function</h2>
+    <?php
+    echo strlen("Hello World");
+    ?>
+</body>
+</html>
---
+++ 05_Function.php
@@ -0,0 +1,26 @@
+<!-- 05_Function.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Function</title>
+</head>
+<body>
+    <h1>function</h1>
+    <?php
+    $str = "Lorem ipsum dolor sit amet consectetur 
+    adipisicing elit. Repudiandae, sed tempora veniam suscipit.";
+    echo $str;
+    ?>
+    <h2>strlen()</h2>
+    <?php
+    echo strlen($str);
+    ?>
+    <h2>nl2br</h2>
+    <?php
+    echo nl2br($str);
+    ?>
+</body>
+</html>
