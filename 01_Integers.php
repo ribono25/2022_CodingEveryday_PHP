725dc300469415f08383225ba27f802ddf7d6fb3
SeongHyeon Kim <94503162+ribono25@users.noreply.github.com>
2022-04-06 09:06:56 +0900

    Add files via upload

---
+++ 01_Integers.php
@@ -0,0 +1,29 @@
+<!-- 01_Integers.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Integers & Arithmetic Operators</h1>
+    <h2>1+1</h2>
+    <?php
+    echo 1+1;
+    ?>
+    <h2>2-1</h2>
+    <?php
+    echo 2-1;
+    ?>
+    <h2>2*2</h2>
+    <?php
+    echo 2*2;
+    ?>
+    <h2>4/2</h2>
+    <?php
+    echo 4/2;
+    ?>
+</body>
+</html>
